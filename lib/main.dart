1931382288f24e90544341ccfb0a1d54f4f6616c
Lee Walker <leewalker@outlook.com>
2026-01-24 15:28:30 -0500

    Adding api key details

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ttrpg_sim/features/game/presentation/game_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const ProviderScope(child: TtrpgSimApp()));
 }
 
